6984962fb54056a7cd32ddc811667c9677a1483b
Sanu Singh Rajput <81958742+Sanu-Singh@users.noreply.github.com>
2024-05-29 15:21:53 +0530

    Update model.py

--- Drowsiness detection/model.py
+++ Drowsiness detection/model.py
@@ -1,15 +1,16 @@
 import os
 from keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt 
 import numpy as np
-from keras.utils.np_utils import to_categorical
+from keras.utils import to_categorical
 import random,shutil
 from keras.models import Sequential
 from keras.layers import Dropout,Conv2D,Flatten,Dense, MaxPooling2D, BatchNormalization
 from keras.models import load_model
 
 
-def generator(dir, gen=image.ImageDataGenerator(rescale=1./255), shuffle=True,batch_size=1,target_size=(24,24),class_mode='categorical' ):
+def generator(dir, gen=ImageDataGenerator(rescale=1./255), shuffle=True, batch_size=1, target_size=(24,24), class_mode='categorical'):
 
     return gen.flow_from_directory(dir,batch_size=batch_size,shuffle=shuffle,color_mode='grayscale',class_mode=class_mode,target_size=target_size)
 
@@ -47,11 +48,11 @@ model = Sequential([
 #one more dropout for convergence' sake :) 
     Dropout(0.5),
 #output a softmax to squash the matrix into output probabilities
-    Dense(2, activation='softmax')
+    Dense(4, activation='softmax')
 ])
 
 model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
-model.fit_generator(train_batch, validation_data=valid_batch,epochs=15,steps_per_epoch=SPE ,validation_steps=VS)
+model.fit(train_batch, validation_data=valid_batch,epochs=50,steps_per_epoch=SPE ,validation_steps=VS)
 
-model.save('models/cnnCat2.h5', overwrite=True)
+model.save('models/cnnCat2.keras', overwrite=True)
